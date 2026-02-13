f4a8a6b4e7bb959925ad5713aec1e378d2e555f9
caio costa <caiocosta9101@gmail.com>
2026-02-13 12:55:43 -0300

    url atualizada

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,9 @@
+/// <reference types="vite/client" />
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  // O SEGREDO: Se tiver variável da Vercel, usa ela. Senão, usa localhost.
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
 });
 
 export default api;
